b03b97e2dde77a8d3a725d538621be2b2ad3579b
Hilmasfa11 <hilmasitifadilahamalia@upi.edu>
2023-12-07 11:32:16 +0700

    update uts

---
+++ main.js
@@ -0,0 +1,32 @@
+document.getElementById("myButton1").addEventListener("click", function() {
+    var textElement = document.getElementById("myText1");
+
+    // Toggle the display property of the text element
+    if (textElement.style.display === "none") {
+        textElement.style.display = "block";
+    } else {
+        textElement.style.display = "none";
+    }
+});
+
+document.getElementById("myButton2").addEventListener("click", function() {
+    var textElement = document.getElementById("myText2");
+
+    // Toggle the display property of the text element
+    if (textElement.style.display === "none") {
+        textElement.style.display = "block";
+    } else {
+        textElement.style.display = "none";
+    }
+});
+
+document.getElementById("myButton3").addEventListener("click", function() {
+    var textElement = document.getElementById("myText3");
+
+    // Toggle the display property of the text element
+    if (textElement.style.display === "none") {
+        textElement.style.display = "block";
+    } else {
+        textElement.style.display = "none";
+    }
+});
